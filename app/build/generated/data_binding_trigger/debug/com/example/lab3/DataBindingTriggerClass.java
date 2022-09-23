0e93653c0aff895fcb4f0e8254a654cd2cce9a09
yingleifang <55908629+yingleifang@users.noreply.github.com>
2022-09-23 12:48:55 -0500

    easy mode hard mode set up

---
+++ app/build/generated/data_binding_trigger/debug/com/example/lab3/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.lab3;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.lab3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lab3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.easyModeButton.setOnClickListener { launchEasy() }
+
+        // Launch the HorizontalListActivity on horizontalBtn click
+        binding.hardModeButton.setOnClickListener { launchHard() }
+    }
+
+    private fun launchEasy() {
+        startActivity(Intent(this@MainActivity, MultipleChoiceQuestionActivity::class.java))
+    }
+
+    private fun launchHard() {
+        startActivity(Intent(this@MainActivity, MultipleChoiceQuestionActivity::class.java))
     }
 }
